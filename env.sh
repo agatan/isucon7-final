7d29a3ddb1ff2d2d0518feadd5cf549089afa39a
Junya Ogasawara <oga.ivc.s27@gmail.com>
2017-11-25 17:27:20 +0900

    Disable debug env

--- env.sh
+++ env.sh
@@ -4,5 +4,3 @@ ISU_DB_PASSWORD=isucon
 ISU_WEB_HOSTS=app0101.isu7f.k0y.org:5000,app0102.isu7f.k0y.org:5000,app0103.isu7f.k0y.org:5000
 REDIS_URL=redis://localhost:6379
 SHARED_REDIS_URL=redis://192.168.10.4:6379
-
-DEBUG=1
