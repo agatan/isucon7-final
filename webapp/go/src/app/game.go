11d1f5a4936014c3265a25ddc37c36163c69ab90
izumin5210 <m@izum.in>
2017-11-25 17:12:34 +0900

    Remove unneeded txn

--- webapp/go/src/app/game.go
+++ webapp/go/src/app/game.go
@@ -193,15 +193,15 @@ func buyItem(roomName string, itemID int, countBought int, reqTime int64) bool {
 	mu.Lock()
 	defer mu.Unlock()
 
-	tx, err := db.Beginx()
-	if err != nil {
-		log.Println(err)
-		return false
-	}
+	// tx, err := db.Beginx()
+	// if err != nil {
+	//   log.Println(err)
+	//   return false
+	// }
 
 	_, ok := updateRoomTime(tx, roomName, reqTime)
 	if !ok {
-		tx.Rollback()
+		// tx.Rollback()
 		return false
 	}
 
@@ -210,11 +210,11 @@ func buyItem(roomName string, itemID int, countBought int, reqTime int64) bool {
 	// err = tx.Get(&countBuying, "SELECT COUNT(*) FROM buying WHERE room_name = ? AND item_id = ?", roomName, itemID)
 	if err != nil {
 		log.Println(err)
-		tx.Rollback()
+		// tx.Rollback()
 		return false
 	}
 	if countBuying != countBought {
-		tx.Rollback()
+		// tx.Rollback()
 		log.Println(roomName, itemID, countBought+1, " is already bought")
 		return false
 	}
@@ -238,7 +238,7 @@ func buyItem(roomName string, itemID int, countBought int, reqTime int64) bool {
 	// err = tx.Select(&buyings, "SELECT item_id, ordinal, time FROM buying WHERE room_name = ?", roomName)
 	if err != nil {
 		log.Println(err)
-		tx.Rollback()
+		// tx.Rollback()
 		return false
 	}
 	for _, b := range buyings {
@@ -255,7 +255,7 @@ func buyItem(roomName string, itemID int, countBought int, reqTime int64) bool {
 	need := new(big.Int).Mul(item.GetPrice(countBought+1), big.NewInt(1000))
 	if totalMilliIsu.Cmp(need) < 0 {
 		log.Println("not enough")
-		tx.Rollback()
+		// tx.Rollback()
 		return false
 	}
 
@@ -268,14 +268,14 @@ func buyItem(roomName string, itemID int, countBought int, reqTime int64) bool {
 	// _, err = tx.Exec("INSERT INTO buying(room_name, item_id, ordinal, time) VALUES(?, ?, ?, ?)", roomName, itemID, countBought+1, reqTime)
 	if err != nil {
 		log.Println(err)
-		tx.Rollback()
+		// tx.Rollback()
 		return false
 	}
 
-	if err := tx.Commit(); err != nil {
-		log.Println(err)
-		return false
-	}
+	// if err := tx.Commit(); err != nil {
+	//   log.Println(err)
+	//   return false
+	// }
 
 	return true
 }
