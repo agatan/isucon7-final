63549d4e5829dcc3f7ac523497ecc3170f3ae7c3
Junya Ogasawara <oga.ivc.s27@gmail.com>
2017-11-25 11:36:05 +0900

    Add redis_url

--- env.sh
+++ env.sh
@@ -2,5 +2,6 @@ ISU_DB_HOST=app0104
 ISU_DB_USER=isucon
 ISU_DB_PASSWORD=isucon
 ISU_WEB_HOSTS=app0101.isu7f.k0y.org,app0102.isu7f.k0y.org,app0103.isu7f.k0y.org
+REDIS_URL=redis://192.168.10.4:637
 
 DEBUG=1
